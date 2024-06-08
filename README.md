34811bff7dd3375922cf9f69abc001ae21f16093
rtfmkiesel <79413747+rtfmkiesel@users.noreply.github.com>
2024-06-08 09:55:21 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stickers
+Some random stickers I designed. If not specified otherwise, they are licensed under the CC0 1.0 Universal.
+
+![The unencrypted_no_thanks_DE sticker from github.com/rtfmkiesel/Stickers](./previews/unencrypted_no_thanks_DE.png)
+![The unencrypted_no_thanks_EN sticker from github.com/rtfmkiesel/Stickers](./previews/unencrypted_no_thanks_EN.png)
+![The exploit_content sticker from github.com/rtfmkiesel/Stickers](./previews/exploit_content.png)
+![The encryption_advisory sticker from github.com/rtfmkiesel/Stickers](./previews/encryption_advisory.png)
---
+++ generate_preview.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+svgs=$(find . -type f -name "*.svg")
+for svg in $svgs; do
+	filename=$(basename "$svg")
+	name="${filename%.*}"
+	tmpoutfile="/tmp/$name.png"
+	outfile="./previews/$name.png"
+
+	if ! [ -f "$outfile" ]; then
+		echo "Generating preview for $svg"
+		
+		# SVG to PNG
+		inkscape --export-type=png "$svg" -d 300 -o "$tmpoutfile"
+		# Preview text
+		convert "$tmpoutfile" -fill blue -pointsize 125 -gravity center -draw "rotate -45 text 0,0 'github.com/rtfmkiesel/Stickers, CC0 1.0 Universal'" "$outfile"
+		# Remove possible metadata
+		exiftool -overwrite_original -all= "$outfile"
+		# Add to README
+		echo "![The $name sticker from github.com/rtfmkiesel/Stickers]($outfile)" >> README.md
+		
+		rm "$tmpoutfile"
+	else
+		echo "Skipping $svg, already exists"
+	fi
+done
